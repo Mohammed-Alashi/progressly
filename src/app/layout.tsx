7b3a1bd094ab953408df3722a0d8c92ef0c8cb41
itu-itis25-Mohammed-Alashi <alashim24@itu.edu.tr>
2026-08-27 11:03:10 +0300

    Change the title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "CareerFlow AI",
+  title: "Progressly",
   description: "Your AI career progress assistant",
 };
 
